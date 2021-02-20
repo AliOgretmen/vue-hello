4cd877d3f1bbaeaf6ea3966b7e95ae760e2d13e9
AliOgretmen <aliogretmen.ch@gmail.com>
2021-02-20 17:07:31 +0100

    binding attr, classes and styles

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,63 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div> {{ greet }} {{ name }} </div>
+  <div v-text="date.getFullYear()"></div>
+  <div v-html="country"></div>
+  <h2 v-bind:id="headingid">Heading (2)</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">Underlined Text</h2>
+  <h3 class="underline" v-bind:class="status">Status</h3>
+  <h2 v-bind:class="isPromoted && 'promoted'"> Promoted Code</h2>
+  <h2 v-bind:class="isNew ? 'new' : 'old'"> New Text</h2>
+  <h2 v-bind:class="['new', 'underline', 'promoted']"> Some Text</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted',isNew ? 'new' : 'old']"> Array Text</h2>
+  <h2 v-bind:class="{
+    promoted: isPromoted,
+    old: !isNew,
+    'new' : isNew
+  }"> Object Text</h2>
+  <h2 v-bind:style="{
+    color: highlightCollor,
+    'font-size': headerSize + 'px',
+    padding: '20px'
+  }"> Inline Style Text</h2>
+    <h2 v-bind:style="headerObjectStyle"> Object Style Text</h2>
+    <h2 :style="[baseObjectStyle, successObjectStyle]"> Array Style Text</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){  
+    return{
+      greet: "Hello",
+      name: "Vue",
+      date: new Date,
+      country: "<b>Switzerland</b>",
+      headingid: "Heading-id",
+      isDisabled: true,
+      status: "danger",
+      isPromoted:true,
+      isNew: true,
+      highlightCollor: "orange",
+      headerSize: 50,
+      headerObjectStyle: {
+        color: 'orange',
+        fontSize: '50px',
+        padding: '20px'
+      },
+      baseObjectStyle: {
+        fontSize: '50px',
+        padding: '10px'
+      },
+      successObjectStyle: {
+        color: 'green',
+        backgroundColor: 'lightgreen',
+        border: '2px solid blue'
+      }
+
+    };
+  },  
 }
 </script>
 
@@ -22,5 +69,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline{
+  text-decoration: underline;
+}
+
+.promoted{
+  font-style: italic;
+}
+
+.new{
+  color: green;
+}
+
+.old{
+  color: red;
 }
 </style>
